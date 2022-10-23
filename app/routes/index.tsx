c6857f95d2b5289cd831e144688c21f78c8ea66a
Brian Steele <brian.steele@hey.com>
2022-10-22 17:30:27 -0700

    tailwind and postcss are up and running

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,10 @@ import { useOptionalUser } from "~/utils";
 
 export default function Index() {
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+    <main className="relative min-h-screen sm:flex sm:items-center sm:justify-center">
+      <p className="font-sans font-normal text-white">
+        Testing if this worked. I bet not
+      </p>
       <div className="relative sm:pb-16 sm:pt-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["DMSans", "Arial"],
+      },
+    },
   },
   plugins: [],
 };
